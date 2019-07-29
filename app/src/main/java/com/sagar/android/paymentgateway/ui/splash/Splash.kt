26cdd54e2fcf48dad667192813727d3d9bb05921
SAGAR <snkumar.nayak@gmail.com>
2019-07-29 22:11:07 +0530

    commit

--- app/src/main/java/com/sagar/android/paymentgateway/ui/splash/Splash.kt
+++ app/src/main/java/com/sagar/android/paymentgateway/ui/splash/Splash.kt
@@ -2,6 +2,7 @@ package com.sagar.android.paymentgateway.ui.splash
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.sagar.android.paymentgateway.R
 import kotlinx.android.synthetic.main.activity_splash.*
 import org.kodein.di.KodeinAware
@@ -12,10 +13,17 @@ class Splash : AppCompatActivity(), KodeinAware {
     override val kodein by org.kodein.di.android.kodein()
 
     private val viewModelProvider: SplashViewModelProvider by instance()
+    private lateinit var binding: com.sagar.android.paymentgateway.databinding.ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         setSupportActionBar(toolbar)
+
+        binding = DataBindingUtil.setContentView(
+            this,
+            R.layout.activity_splash
+        )
+        binding.context = this
     }
 }
